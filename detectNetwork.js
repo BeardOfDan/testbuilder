9aa4c886920c0d833bc95f73abb546cb2ceabaa9
Daniel Roberts <BeardOfDan@gmail.com>
2017-09-03 13:15:52 -0700

    Resolve Indentation Issue
    Fixed an issue with my IDE (an extension for it actually) and now
    my indentation is always 2 spaces (instead of 4).

--- detectNetwork.js
+++ detectNetwork.js
@@ -1,41 +1,41 @@
 "use strict";
 
-var detectNetwork = function(cardNumber) {
-    // shorter to type
-    const length = cardNumber.length;
-    const prefix = cardNumber.slice(0, 2);
+var detectNetwork = function (cardNumber) {
+  // shorter to type
+  const length = cardNumber.length;
+  const prefix = cardNumber.slice(0, 2);
 
-    switch (length) {
-        case 14:
-            if ((prefix === "38") || (prefix === "39")) {
-                return "Diner's Club";
-            }
-            break;
+  switch (length) {
+    case 14:
+      if ((prefix === "38") || (prefix === "39")) {
+        return "Diner's Club";
+      }
+      break;
 
-        case 15:
-            if ((prefix === "34") || (prefix === "37")) {
-                return "American Express";
-            }
-            break;
+    case 15:
+      if ((prefix === "34") || (prefix === "37")) {
+        return "American Express";
+      }
+      break;
 
-        case 13:
-        case 19:
-            if (prefix.charAt(0) === "4") {
-                return "Visa";
-            }
-            break;
+    case 13:
+    case 19:
+      if (prefix.charAt(0) === "4") {
+        return "Visa";
+      }
+      break;
 
-        case 16:
-            if (prefix.charAt(0) === "4") {
-                return "Visa";
-            } else {
-                prefixNum = new Number(prefix);
-                if ((prefixNum < 56) && (prefixNum > 50)) {
-                    return "MasterCard";
-                }
-            }
-            break;
-    } // end of switch
+    case 16:
+      if (prefix.charAt(0) === "4") {
+        return "Visa";
+      } else {
+        prefixNum = new Number(prefix);
+        if ((prefixNum < 56) && (prefixNum > 50)) {
+          return "MasterCard";
+        }
+      }
+      break;
+  } // end of switch
 
 };
 
@@ -59,23 +59,23 @@ let values = suppliedTestString.split("\n");
 // Since the instructions only state to work in this file 
 // (and the console), instead of doing anything in the .test.js file (yet),
 // I am doing some basic testing this way
-pseudoTests = function() {
-    let passes = 0;
-    let failures = 0;
+pseudoTests = function () {
+  let passes = 0;
+  let failures = 0;
 
-    for (let i = 0; i < values.length; i++) {
-        let cardNumber = values[i].match(/\d+/)[0];
-        let expected = values[i].match(/\(.+\)/)[0];
-        // remove the parentheses
-        expected = expected.slice(1, expected.length - 1);
+  for (let i = 0; i < values.length; i++) {
+    let cardNumber = values[i].match(/\d+/)[0];
+    let expected = values[i].match(/\(.+\)/)[0];
+    // remove the parentheses
+    expected = expected.slice(1, expected.length - 1);
 
-        let actual = detectNetwork(cardNumber);
+    let actual = detectNetwork(cardNumber);
 
-        if (actual === expected) {
-            passes++;
-        } else {
-            failures++;
-        }
+    if (actual === expected) {
+      passes++;
+    } else {
+      failures++;
     }
-    console.log(`${passes} tests passing \n${failures} tests failing`);
+  }
+  console.log(`${passes} tests passing \n${failures} tests failing`);
 };
