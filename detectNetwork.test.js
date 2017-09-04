cbc0175b8457b48eb8242e535bd58ed9737fb5a8
Daniel Roberts <BeardOfDan@gmail.com>
2017-09-04 15:25:02 -0700

    Add tests for "China UnionPay" and "Switch"

--- detectNetwork.test.js
+++ detectNetwork.test.js
@@ -1,101 +1,3 @@
-
-describe('Diner\'s Club', function () {
-  // Be careful, tests can have bugs too...
-
-  it('has a prefix of 38 and a length of 14', function () {
-    if (detectNetwork('38345678901234') !== 'Diner\'s Club') {
-      throw new Error('Test failed');
-    }
-  });
-
-  it('has a prefix of 39 and a length of 14', function () {
-    if (detectNetwork('39345678901234') !== 'Diner\'s Club') {
-      throw new Error('Test failed');
-    }
-
-  });
-});
-
-describe('American Express', function () {
-  // It can get annoying to keep typing the if/throw, so here is a
-  // helper function to throw an error if the input statement isn't true. 
-  var assert = function (isTrue) {
-    if (!isTrue) {
-      throw new Error('Test failed');
-    }
-  };
-
-  it('has a prefix of 34 and a length of 15', function () {
-    assert(detectNetwork('343456789012345') === 'American Express');
-  });
-
-  it('has a prefix of 37 and a length of 15', function () {
-    assert(detectNetwork('373456789012345') === 'American Express');
-  });
-});
-
-describe('Visa', function () {
-  // Chai is an entire library of helper functions for tests!
-  // Chai provides an assert that acts the same as our previous assert.
-  // Search the documentation to figure out how to access it. 
-  //   http://chaijs.com/
-  var assert = chai.assert;
-
-
-  it('has a prefix of 4 and a length of 13', function () {
-    assert(detectNetwork('4123456789012') === 'Visa');
-  });
-
-  it('has a prefix of 4 and a length of 16', function () {
-    assert(detectNetwork('4123456789012345') === 'Visa');
-  });
-
-  it('has a prefix of 4 and a length of 19', function () {
-    assert(detectNetwork('4123456789012345678') === 'Visa');
-  });
-});
-
-describe('MasterCard', function () {
-  // Chai lets you write more human-readable tests that throw helpful errors.
-  // Expect syntax is one way to do this, but there are others. 
-  // If you want to know more, check out the documentation. 
-  //   http://chaijs.com/api/bdd/
-  var expect = chai.expect;
-
-  it("has a prefix of 51 and a length of 16", function () {
-    expect(detectNetwork('5112345678901234')).to.equal('MasterCard');
-  });
-
-  it("has a prefix of 52 and a length of 16", function () {
-    expect(detectNetwork('5212345678901234')).to.equal('MasterCard');
-  });
-
-  it("has a prefix of 53 and a length of 16", function () {
-    expect(detectNetwork('5312345678901234')).to.equal('MasterCard');
-  });
-
-  // You can also use should instead of expect, which changes the style
-  // slightly. It really doesn't matter which one you use - check out 
-  // http://chaijs.com/guide/styles/ for more info, but it's important
-  // to be consistent (unlike in this file, where we use BOTH expect
-  // and should, but that's just for learning), so once you've gotten 
-  // these tests to pass using should syntax, refactor your tests to 
-  // use either expect or should, but not both. 
-  var should = chai.should();
-
-  it('has a prefix of 54 and a length of 16', function () {
-    detectNetwork('5412345678901234').should.equal('MasterCard');
-  });
-
-  it('has a prefix of 55 and a length of 16', function () {
-    detectNetwork('5512345678901234').should.equal('MasterCard');
-  });
-
-});
-
-// The above tests are still hardcoded
-// The below tests are automated with the performTests function
-
 // This function will automatically test all of the different possible combinations
 const performTests = function (prefixes, lengths, expected) {
   const { expect } = chai;
@@ -129,7 +31,36 @@ const performTests = function (prefixes, lengths, expected) {
       })();
     }
   }
-};
+}; // end of performTests(prefixes, lengths, expected)
+
+
+describe('Diner\'s Club', function () {
+  const prefixes = ["38", "39"];
+  const lengths = [14];
+
+  performTests(prefixes, lengths, "Diner's Club");
+});
+
+describe('American Express', function () {
+  const prefixes = ["34", "37"];
+  const lengths = [15];
+
+  performTests(prefixes, lengths, "American Express");
+});
+
+describe('Visa', function () {
+  const prefixes = ["4"];
+  const lengths = [13, 16, 19];
+
+  performTests(prefixes, lengths, "Visa");
+});
+
+describe('MasterCard', function () {
+  const prefixes = ["51", "52", "53", "54", "55"];
+  const lengths = [16];
+
+  performTests(prefixes, lengths, "MasterCard");
+});
 
 describe('Discover', function () {
   const prefixes = ["6011", "644", "645", "646", "647", "648", "649", "65"];
@@ -145,5 +76,20 @@ describe('Maestro', function () {
   performTests(prefixes, lengths, "Maestro");
 });
 
-describe('should support China UnionPay')
-describe('should support Switch')
+describe('China UnionPay', function () {
+  // China UnionPay always has a prefix of 622126-622925, 624-626, 
+  // or 6282-6288 and a length of 16-19.
+  const prefixes = ["622126-622925", "624-626", "6282-6288"];
+  const lengths = [16, 17, 18, 19];
+
+  performTests(prefixes, lengths, "China UnionPay");
+});
+
+describe('Switch', function () {
+  // Switch always has a prefix of 4903, 4905, 4911, 4936, 
+  // 564182, 633110, 6333, or 6759 and a length of 16, 18, or 19
+  const prefixes = ["4903", "4905", "4911", "4936", "564182", "633110", "6333", "6759"];
+  const lengths = [16, 18, 19];
+
+  performTests(prefixes, lengths, "Switch");
+});
