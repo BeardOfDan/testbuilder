f6e5acbf0b62fdc714a64cb82a68aa9cfe41d667
Daniel Roberts <BeardOfDan@gmail.com>
2017-09-03 18:10:01 -0700

    Do all testing in detectNetwork.test.js
    Removed all testing from detectNetwork.js and fixed the first few
    tests in detectNetwork.test.js

--- detectNetwork.js
+++ detectNetwork.js
@@ -35,47 +35,8 @@ var detectNetwork = function (cardNumber) {
         }
       }
       break;
+
+    default:
+      return "Unhandled Card Number: " + cardNumber;
   } // end of switch
-
-};
-
-// The tests given in the console are all of a certain format
-// so I will make a function that will convert them into an array
-let suppliedTestString = `38345678901234 (Diner's Club)
-39345678901234 (Diner's Club)
-343456789012345 (American Express)
-373456789012345 (American Express)
-4123456789012 (Visa)
-4123456789012345 (Visa)
-4123456789012345678 (Visa)
-5112345678901234 (MasterCard)
-5212345678901234 (MasterCard)
-5312345678901234 (MasterCard)
-5412345678901234 (MasterCard)
-5512345678901234 (MasterCard)`;
-
-let values = suppliedTestString.split("\n");
-
-// Since the instructions only state to work in this file 
-// (and the console), instead of doing anything in the .test.js file (yet),
-// I am doing some basic testing this way
-const pseudoTests = function () {
-  let passes = 0;
-  let failures = 0;
-
-  for (let i = 0; i < values.length; i++) {
-    let cardNumber = values[i].match(/\d+/)[0];
-    let expected = values[i].match(/\(.+\)/)[0];
-    // remove the parentheses
-    expected = expected.slice(1, expected.length - 1);
-
-    let actual = detectNetwork(cardNumber);
-
-    if (actual === expected) {
-      passes++;
-    } else {
-      failures++;
-    }
-  }
-  console.log(`${passes} tests passing \n${failures} tests failing`);
-};
+}; // end of detectNetwork(cardNumber)
